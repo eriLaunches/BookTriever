c797e32c7a4544d64d5d311a420cbcbe44b21020
Erica Luong <huongie2lu@gmail.com>
2019-01-03 13:23:47 -0500

    Initial commit

---
+++ client/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    console.log('hiiii')
+    return <div>hiiiiiii</div>
+  }
+}
+
+export default Main
