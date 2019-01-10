0b03416897920d4ab254ef10490da8b1badef281
Erica Luong <huongie2lu@gmail.com>
2019-01-10 11:05:00 -0500

    Documentation cleanUp

--- client/utilities/identifierHelper.js
+++ client/utilities/identifierHelper.js
@@ -1,4 +1,4 @@
-/* Helper function to identify the id in the book object to pass id into Open Library Book API to fetch book description. This function first looks for if the object contains an isbn. If it does, it will take the first isbn in the array as the id. Else if the object contains an oclc, then it will take the first oclc in the array as the id. And so on... If there are no ids matching the criteria, the function will return identifier as undefined */
+/* Helper function to identify the id in the book object to pass id into Open Library Book API to fetch book description. This function first looks for if the object contains an isbn. If it does, it will take the first isbn in the array as the id and update the identifier object. Else if the object contains an oclc, then it will take the first oclc in the array as the id. And so on... If there are no ids matching the criteria, the function will return identifier as undefined */
 
 export const identifierHelper = book => {
   let identifier = undefined
